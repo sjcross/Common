6a5710dc69a9ddbd007ad9782f3d28f4db9aa2e4
SJ Cross <sc13967@bristol.ac.uk>
2016-07-19 11:15:58 +0100

    Initial commit

---
+++ src/main/java/com/wolfson/ArrayFunc/ArrayFunc.java
@@ -0,0 +1,191 @@
+package com.wolfson.ArrayFunc;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class ArrayFunc
+{
+	public static int[] uniqueVals (int[] col_vals) {
+		//Unique values from the input int array "col_vals"
+		ArrayList<Integer> un_al = new ArrayList<Integer>();
+		for (int i=0;i<col_vals.length;i++){
+			if (!un_al.contains(col_vals[i])) {
+				un_al.add(col_vals[i]);
+			}			
+		}
+
+		int[] un_ar = new int[un_al.size()];
+		for (int i=0;i<un_al.size();i++) {
+			un_ar[i] = (int) un_al.get(i);
+		}
+		return un_ar;
+	}
+
+	public static float[] uniqueVals (float[] col_vals) {
+		//Unique values from the input float array "col_vals"
+		ArrayList<Float> un_al = new ArrayList<Float>();
+		for (int i=0;i<col_vals.length;i++){
+			if (!un_al.contains(col_vals[i])) {
+				un_al.add(col_vals[i]);
+			}			
+		}
+
+		float[] un_ar = new float[un_al.size()];
+		for (int i=0;i<un_al.size();i++) {
+			un_ar[i] = (float) un_al.get(i);
+		}
+		return un_ar;
+	}
+
+	public static double[] uniqueVals (double[] col_vals) {
+		//Unique values from the input double array "col_vals"
+		ArrayList<Double> un_al = new ArrayList<Double>();
+		for (int i=0;i<col_vals.length;i++){
+			if (!un_al.contains(col_vals[i])) {
+				un_al.add(col_vals[i]);
+			}			
+		}
+
+		double[] un_ar = new double[un_al.size()];
+		for (int i=0;i<un_al.size();i++) {
+			un_ar[i] = (double) un_al.get(i);
+		}
+		return un_ar;
+	}
+
+	public static int[] uniqueRows(int[] col_vals) {
+		//Rows corresponding to unique values from the input int array "col_vals"
+		ArrayList<Integer> un_al = new ArrayList<Integer>();
+		ArrayList<Integer> r_al = new ArrayList<Integer>();
+		for (int i=0;i<col_vals.length;i++){
+			if (!un_al.contains(col_vals[i])) {
+				un_al.add(col_vals[i]);
+				r_al.add(i);
+			}			
+		}
+
+		int[] r_ar = new int[r_al.size()];
+		for (int i=0;i<r_al.size();i++) {
+			r_ar[i] = (int) r_al.get(i);
+		}
+		return r_ar;
+	}
+
+	public static int[] correspondingRows(double[] col_vals, double tar) {
+		//Rows corresponding to target ("tar") values from the input float array "col_vals"
+		ArrayList<Integer> corr_al = new ArrayList<Integer>();
+		for (int i=0;i<col_vals.length;i++){
+			if (col_vals[i]==tar) {
+				corr_al.add(i);
+			}			
+		}
+
+		int[] corr_ar = new int[corr_al.size()];
+		for (int i=0;i<corr_al.size();i++) {
+			corr_ar[i] = (int) corr_al.get(i);
+		}
+
+		return corr_ar;
+	}
+
+	public static double[] getSubArray(double[] col_vals, int[] rows) {
+		double[] arr_out = new double[rows.length];
+		for (int i=0;i<rows.length;i++) {
+			arr_out[i] = col_vals[rows[i]];
+		}
+		return arr_out;
+	}
+
+	public static double getMinIncrement(double[] col_vals) {
+		double inc = Double.POSITIVE_INFINITY;
+
+		for (int i=0;i<col_vals.length-1;i++) {
+			if (Math.abs(col_vals[i+1]-col_vals[i])<inc) {
+				BigDecimal a = BigDecimal.valueOf(col_vals[i+1]);
+				BigDecimal b = BigDecimal.valueOf(col_vals[i]);
+				double c = a.subtract(b).doubleValue();
+				inc = Math.abs(c);
+			}
+		}
+		return inc;
+	}
+
+	public static double[] padArray(double[] arr_in, int l, double pad) {
+		double[] arr_out = new double[l];
+		for (int i=0;i<l;i++) {
+			if (i < arr_in.length) {
+				arr_out[i] = arr_in[i];
+			} else {
+				arr_out[i] = pad;
+			}
+		}
+		return arr_out;
+	}
+
+	public static String[] concatenateArrays(String[] arr_1, String[] arr_2) {
+		String[] arr_out = new String[arr_1.length+arr_2.length];
+
+		for (int i=0;i<arr_1.length;i++) {
+			arr_out[i] = arr_1[i];
+		}
+		for (int i=0;i<arr_2.length;i++) {
+			arr_out[i+arr_1.length] = arr_2[i];
+		}
+
+		return arr_out;
+	}
+
+	public static boolean contains(int[] list, int test) {
+		Boolean boo = false;
+
+		for (int i=0;i<list.length;i++) {
+			if (list[i]==test) {
+				boo = true;
+			}
+		}
+
+		return boo;
+	}
+
+	public static int sum(int[] arr) {
+		int out = 0;
+		for (int i=0;i<arr.length;i++) {
+			out += arr[i];
+		}
+		return out;
+	}
+
+	public static int max(int[] arr) {
+		int out = 0;
+		for (int i=0;i<arr.length;i++) {
+			if (arr[i]>out) {
+				out = arr[i];
+			}
+		}
+		return out;
+	}
+
+	public static double[] arrayListToArray(ArrayList<Double> al) {
+		double[] ar = new double[al.size()];
+		for (int i=0;i<ar.length;i++) {
+			ar[i] = al.get(i); 
+		}
+		return ar;
+	}
+	
+	public static int[] incrementedNumbers(int start, int end) {
+		int[] out = new int[end-start];
+		for(int i=0;i<out.length;i++) {
+			out[i] = start+i;
+		}
+		return out;
+	}
+	
+	public static double[] incrementedNumbers(double start, double end) {
+		double[] out = new double[(int) (end-start)];
+		for(int i=0;i<out.length;i++) {
+			out[i] = start+i;
+		}
+		return out;
+	}
+}
---
+++ src/main/java/com/wolfson/CumStat/CumStat.java
@@ -0,0 +1,142 @@
+package com.wolfson.CumStat;
+
+import java.util.Arrays;
+
+// THIS CLASS IS BASED ON THE INCREMENTAL CALCULATION OF WEIGHTED MEAN AND VARIANCE FROM http://www-uxsup.csx.cam.ac.uk/~fanf2/hermes/doc/antiforgery/stats.pdf (Accessed 30-06-2016)
+
+public class CumStat {
+
+	double[] x_mean;
+	double[] x_var;
+	double[] n;
+	double[] w_sum;
+	double[] S;
+	double[] x_sum;
+	double[] x_min;
+	double[] x_max;
+
+	public CumStat(int len) {
+		x_mean = new double[len];
+		x_var = new double[len];
+		n = new double[len];
+		w_sum = new double[len];
+		S = new double[len];
+		x_sum = new double[len];
+		x_min = new double[len];
+		Arrays.fill(x_min, Double.POSITIVE_INFINITY);
+		x_max = new double[len];
+		Arrays.fill(x_max, Double.NEGATIVE_INFINITY);
+	}
+
+	public void addMeasure(double[] x_in) { //Non-weighted vector
+		for (int i=0;i<x_in.length;i++) {
+			n[i] ++;
+			x_sum[i] = x_sum[i] + x_in[i];
+			w_sum[i] = w_sum[i] + 1;
+			double x_mean_prev = x_mean[i];
+			x_mean[i] = x_mean[i] + (1/w_sum[i])*(x_in[i]-x_mean[i]);
+			S[i] = (S[i]+(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]));
+			x_var[i] = S[i]/w_sum[i];
+			
+			if (x_in[i] < x_min[i]) {
+				x_min[i] = x_in[i];
+			}
+			
+			if (x_in[i] > x_max[i]) {
+				x_max[i] = x_in[i];
+			}
+		}
+	}
+
+	public void addMeasure(double x_in) { //Non-weighted value
+		n[0] ++;
+		x_sum[0] = x_sum[0] + x_in;
+		w_sum[0] = w_sum[0] + 1;
+		double x_mean_prev = x_mean[0];
+		x_mean[0] = x_mean[0] + (1/w_sum[0])*(x_in-x_mean[0]);
+		S[0] = (S[0]+(x_in-x_mean_prev)*(x_in-x_mean[0]));
+		x_var[0] = S[0]/w_sum[0];
+		
+		if (x_in < x_min[0]) {
+			x_min[0] = x_in;
+		}
+		
+		if (x_in > x_max[0]) {
+			x_max[0] = x_in;
+		}
+	}
+
+	public void addMeasure(double[] x_in, double[] w) {//Weighted vector
+		for (int i=0;i<x_mean.length;i++) {
+			if (w[i] != 0) {
+				n[i] ++;
+				x_sum[i] = x_sum[i] + x_in[i];
+				w_sum[i] = w_sum[i] + w[i];
+				double x_mean_prev = x_mean[i];
+				x_mean[i] = x_mean[i] + (w[i]/w_sum[i])*(x_in[i]-x_mean[i]);
+				S[i] = (S[i]+w[i]*(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]));
+				x_var[i] = S[i]/w_sum[i];
+				
+				if (x_in[i] < x_min[i]) {
+					x_min[i] = x_in[i];
+				}
+				
+				if (x_in[i] > x_max[i]) {
+					x_max[i] = x_in[i];
+				}
+			}
+		}
+	}
+
+	public void addMeasure(double x_in, double w) {//Weighted value
+		if (w != 0) {
+			n[0] ++;
+			x_sum[0] = x_sum[0] + x_in;
+			w_sum[0] = w_sum[0] + w;
+			double x_mean_prev = x_mean[0];
+			x_mean[0] = x_mean[0] + (w/w_sum[0])*(x_in-x_mean[0]);
+			S[0] = (S[0]+w*(x_in-x_mean_prev)*(x_in-x_mean[0]));
+			x_var[0] = S[0]/w_sum[0];
+			
+			if (x_in < x_min[0]) {
+				x_min[0] = x_in;
+			}
+			
+			if (x_in > x_max[0]) {
+				x_max[0] = x_in;
+			}
+		}
+	}
+
+	public double[] getMean() {
+		return x_mean;
+	}
+
+	public double[] getSum() {
+		return x_sum;
+	}
+
+	public double[] getVar() {
+		return x_var;
+	}
+
+	public double[] getStd() {
+		double[] x_std = new double[x_var.length];
+		for (int i=0;i<x_var.length;i++) {
+			x_std[i] = Math.sqrt(x_var[i]);
+		}
+		return x_std;
+	}
+
+	public double[] getN() {
+		return n;
+	}
+
+	public double[] getMin() {
+		return x_min;
+	}
+	
+	public double[] getMax() {
+		return x_max;
+	}
+}
