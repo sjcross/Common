3abbf9d941a68173b36c849c54c49ea1711692d4
SJ Cross <sc13967@bristol.ac.uk>
2016-07-29 16:35:18 +0100

    Minor alterations to CumStat, but nothing that should affect the output.  Added check for NaN values, which previously led to all values being set as NaN.

--- src/main/java/com/wolfson/CumStat/CumStat.java
+++ src/main/java/com/wolfson/CumStat/CumStat.java
@@ -30,57 +30,19 @@ public class CumStat {
 
 	public void addMeasure(double[] x_in) { //Non-weighted vector
 		for (int i=0;i<x_in.length;i++) {
-			n[i] ++;
-			x_sum[i] = x_sum[i] + x_in[i];
-			w_sum[i] = w_sum[i] + 1;
-			double x_mean_prev = x_mean[i];
-			x_mean[i] = x_mean[i] + (1/w_sum[i])*(x_in[i]-x_mean[i]);
-			S[i] = (S[i]+(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]));
-			x_var[i] = S[i]/w_sum[i];
-			
-			if (x_in[i] < x_min[i]) {
-				x_min[i] = x_in[i];
-			}
-			
-			if (x_in[i] > x_max[i]) {
-				x_max[i] = x_in[i];
-			}
-		}
-	}
-
-	public void addMeasure(double x_in) { //Non-weighted value
-		n[0] ++;
-		x_sum[0] = x_sum[0] + x_in;
-		w_sum[0] = w_sum[0] + 1;
-		double x_mean_prev = x_mean[0];
-		x_mean[0] = x_mean[0] + (1/w_sum[0])*(x_in-x_mean[0]);
-		S[0] = (S[0]+(x_in-x_mean_prev)*(x_in-x_mean[0]));
-		x_var[0] = S[0]/w_sum[0];
-		
-		if (x_in < x_min[0]) {
-			x_min[0] = x_in;
-		}
-		
-		if (x_in > x_max[0]) {
-			x_max[0] = x_in;
-		}
-	}
-
-	public void addMeasure(double[] x_in, double[] w) {//Weighted vector
-		for (int i=0;i<x_mean.length;i++) {
-			if (w[i] != 0) {
+			if (!Double.isNaN(x_in[i])) {
 				n[i] ++;
 				x_sum[i] = x_sum[i] + x_in[i];
-				w_sum[i] = w_sum[i] + w[i];
+				w_sum[i] = w_sum[i] + 1;
 				double x_mean_prev = x_mean[i];
-				x_mean[i] = x_mean[i] + (w[i]/w_sum[i])*(x_in[i]-x_mean[i]);
-				S[i] = (S[i]+w[i]*(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]));
+				x_mean[i] = x_mean[i] + (1/w_sum[i])*(x_in[i]-x_mean[i]);
+				S[i] = S[i]+(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]);
 				x_var[i] = S[i]/w_sum[i];
-				
+
 				if (x_in[i] < x_min[i]) {
 					x_min[i] = x_in[i];
 				}
-				
+
 				if (x_in[i] > x_max[i]) {
 					x_max[i] = x_in[i];
 				}
@@ -88,26 +50,72 @@ public class CumStat {
 		}
 	}
 
-	public void addMeasure(double x_in, double w) {//Weighted value
-		if (w != 0) {
+	public void addMeasure(double x_in) { //Non-weighted value
+		if (!Double.isNaN(x_in)) {
 			n[0] ++;
 			x_sum[0] = x_sum[0] + x_in;
-			w_sum[0] = w_sum[0] + w;
+			w_sum[0] = w_sum[0] + 1;
 			double x_mean_prev = x_mean[0];
-			x_mean[0] = x_mean[0] + (w/w_sum[0])*(x_in-x_mean[0]);
-			S[0] = (S[0]+w*(x_in-x_mean_prev)*(x_in-x_mean[0]));
+			x_mean[0] = x_mean[0] + (1/w_sum[0])*(x_in-x_mean[0]);
+			S[0] = S[0]+(x_in-x_mean_prev)*(x_in-x_mean[0]);
 			x_var[0] = S[0]/w_sum[0];
-			
+
 			if (x_in < x_min[0]) {
 				x_min[0] = x_in;
 			}
-			
+
 			if (x_in > x_max[0]) {
 				x_max[0] = x_in;
 			}
 		}
 	}
-	
+
+	public void addMeasure(double[] x_in, double[] w) {//Weighted vector
+		for (int i=0;i<x_mean.length;i++) {
+			if (!Double.isNaN(x_in[i])) {
+				if (w[i] != 0) {
+					n[i] ++;
+					x_sum[i] = x_sum[i] + x_in[i];
+					w_sum[i] = w_sum[i] + w[i];
+					double x_mean_prev = x_mean[i];
+					x_mean[i] = x_mean[i] + (w[i]/w_sum[i])*(x_in[i]-x_mean[i]);
+					S[i] = S[i]+w[i]*(x_in[i]-x_mean_prev)*(x_in[i]-x_mean[i]);
+					x_var[i] = S[i]/w_sum[i];
+
+					if (x_in[i] < x_min[i]) {
+						x_min[i] = x_in[i];
+					}
+
+					if (x_in[i] > x_max[i]) {
+						x_max[i] = x_in[i];
+					}
+				}
+			}
+		}
+	}
+
+	public void addMeasure(double x_in, double w) {//Weighted value
+		if (!Double.isNaN(x_in)) {
+			if (w != 0) {
+				n[0] ++;
+				x_sum[0] = x_sum[0] + x_in;
+				w_sum[0] = w_sum[0] + w;
+				double x_mean_prev = x_mean[0];
+				x_mean[0] = x_mean[0] + (w/w_sum[0])*(x_in-x_mean[0]);
+				S[0] = S[0]+w*(x_in-x_mean_prev)*(x_in-x_mean[0]);
+				x_var[0] = S[0]/w_sum[0];
+
+				if (x_in < x_min[0]) {
+					x_min[0] = x_in;
+				}
+
+				if (x_in > x_max[0]) {
+					x_max[0] = x_in;
+				}
+			}
+		}
+	}
+
 	/**
 	 * Add multiple measurements to a single cumulative value.  Different from addMeasure, which adds a single value to each element of a cumulative array.
 	 */
@@ -116,7 +124,7 @@ public class CumStat {
 			addMeasure(x_in[i]);
 		}
 	}
-	
+
 	/**
 	 * Add multiple measurements with weighting to a single cumulative value.  Different from addMeasure, which adds a single value to each element of a cumulative array.
 	 */
@@ -153,7 +161,7 @@ public class CumStat {
 	public double[] getMin() {
 		return x_min;
 	}
-	
+
 	public double[] getMax() {
 		return x_max;
 	}
