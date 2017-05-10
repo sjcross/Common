ca3ed20a40f8a54b0018fdc7446b3b7138887c5f
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-05-10 23:00:21 +0100

    Added Module to load ImageJ images
    Added new module, which adds image currently open in ImageJ to the highcontent workspace.  This will be useful when using the modules as a standalone (non-BatchProcessor) analysis.

---
+++ src/main/java/wbif/sjx/common/HighContent/Module/ImageJLoader.java
@@ -0,0 +1,32 @@
+package wbif.sjx.common.HighContent.Module;
+
+import ij.IJ;
+import ij.ImagePlus;
+import wbif.sjx.common.HighContent.Object.*;
+
+/**
+ * Created by Stephen on 10/05/2017.
+ */
+public class ImageJLoader implements Module {
+    public static final String OUTPUT_IMAGE = "Output image";
+
+    @Override
+    public void execute(Workspace workspace, ParameterCollection parameters, boolean verbose) {
+        if (verbose) System.out.println("   Loading image from ImageJ");
+
+        // Getting image
+        ImageName outputImageName = (ImageName) parameters.getParameter(this,OUTPUT_IMAGE).getValue();
+        ImagePlus imagePlus = IJ.getImage();
+
+        // Adding image to workspace
+        if (verbose) System.out.println("       Adding image ("+outputImageName+") to workspace");
+        workspace.addImage(outputImageName,new Image(imagePlus));
+
+    }
+
+    @Override
+    public void initialiseParameters(ParameterCollection parameters) {
+        parameters.addParameter(new Parameter(this,Parameter.IMAGE_NAME,OUTPUT_IMAGE,"Im1",false));
+
+    }
+}
