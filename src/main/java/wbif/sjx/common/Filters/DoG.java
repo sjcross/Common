0d8de458f7439b3fdb809a26cd9ed54c2b8982b8
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-06-30 16:13:21 +0100

    Minor fix to DoG filter

--- src/main/java/wbif/sjx/common/Filters/DoG.java
+++ src/main/java/wbif/sjx/common/Filters/DoG.java
@@ -28,6 +28,7 @@ public class DoG {
                         ipl2.setPosition(channel+1,slice+1,frame+1);
 
                         applyDoG(ipl1,ipl2,sigma);
+
                     }
                 }
             }
@@ -36,6 +37,9 @@ public class DoG {
             applyDoG(ipl1,ipl2,sigma);
 
         }
+
+        ipl1.setPosition(1,1,1);
+        ipl2.setPosition(1,1,1);
 
         // Subtracting one from the other to give the final DoG result
         ipl.setImage(new ImageCalculator().run("Subtract create stack 32-bit",ipl2,ipl1));
