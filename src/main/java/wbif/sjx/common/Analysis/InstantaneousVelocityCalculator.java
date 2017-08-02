3e72fb35cb7bc7dfc34f652b375deafb3a80bb18
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-08-02 15:36:13 +0100

    Updated InstantaneousVelocityCalculator
    Now gives the first element of the velocity array as 0, rather than the array being one shorter than the input array lengths.

--- src/main/java/wbif/sjx/common/Analysis/InstantaneousVelocityCalculator.java
+++ src/main/java/wbif/sjx/common/Analysis/InstantaneousVelocityCalculator.java
@@ -5,16 +5,19 @@ package wbif.sjx.common.Analysis;
  */
 public class InstantaneousVelocityCalculator {
     public static double[] calculate(int[] f, double[] x, double[] y, double[] z) {
-        double[] velocity = new double[x.length - 1];
+        double[] velocity = new double[x.length];
+        velocity[0] = 0;
 
         for (int i = 1; i < x.length; i++) {
             double dx = x[i] - x[i - 1];
             double dy = y[i] - y[i - 1];
             double dz = z[i] - z[i - 1];
 
-            velocity[i - 1] = Math.sqrt(dx * dx + dy * dy + dz * dz) / (f[i] - f[i - 1]);
+            velocity[i] = Math.sqrt(dx * dx + dy * dy + dz * dz) / (f[i] - f[i - 1]);
+
         }
 
         return velocity;
+
     }
 }
