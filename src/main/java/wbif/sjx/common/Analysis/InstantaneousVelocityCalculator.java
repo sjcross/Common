d3d68273e3ddfada767ae3f541350ef22fd2ac9e
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-08-02 15:57:00 +0100

    Added DoubleTreeMapConverter class
    This takes an array of interger keys and double values and puts them into a TreeMap.  This is useful for getting Track measures at a specific frame.

--- src/main/java/wbif/sjx/common/Analysis/InstantaneousVelocityCalculator.java
+++ src/main/java/wbif/sjx/common/Analysis/InstantaneousVelocityCalculator.java
@@ -1,5 +1,7 @@
 package wbif.sjx.common.Analysis;
 
+import java.util.TreeMap;
+
 /**
  * Created by steph on 15/04/2017.
  */
@@ -14,6 +16,23 @@ public class InstantaneousVelocityCalculator {
             double dz = z[i] - z[i - 1];
 
             velocity[i] = Math.sqrt(dx * dx + dy * dy + dz * dz) / (f[i] - f[i - 1]);
+
+        }
+
+        return velocity;
+
+    }
+
+    public static TreeMap<Integer,Double> calculateToTree(int[] f, double[] x, double[] y, double[] z) {
+        TreeMap<Integer,Double> velocity = new TreeMap<>();
+
+        for (int i = 1; i < x.length; i++) {
+            double dx = x[i] - x[i - 1];
+            double dy = y[i] - y[i - 1];
+            double dz = z[i] - z[i - 1];
+            double instVelocity = Math.sqrt(dx * dx + dy * dy + dz * dz) / (f[i] - f[i - 1]);
+
+            velocity.put(f[i],instVelocity);
 
         }
 
