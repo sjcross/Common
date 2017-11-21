bea484249ab2bbdde6a5ec76d0d9253178d268b7
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-11-21 18:23:49 +0000

    Added Contrast and Median 3D local thresholds
    Modified Contrast and Median methods from Auto_Local_Threshold to work in 3D.  These have yet to be tested.

--- src/main/java/wbif/sjx/common/Filters/AutoLocalThreshold3D.java
+++ src/main/java/wbif/sjx/common/Filters/AutoLocalThreshold3D.java
@@ -14,7 +14,9 @@ import wbif.sjx.common.Process.IntensityMinMax;
  */
 public class AutoLocalThreshold3D  implements PlugIn {
     public static final String BERNSEN = "Bernsen";
+    public static final String CONTRAST = "Contrast";
     public static final String MEAN = "Mean";
+    public static final String MEDIAN = "Median";
     public static final String PHANSALKAR = "Phansalkar";
 
     public static void main(String[] args) {
@@ -34,8 +36,16 @@ public class AutoLocalThreshold3D  implements PlugIn {
                 Bernsen(ipl,radiusXY,radiusZ,thrMult,par1,par2,doIwhite);
                 break;
 
+            case CONTRAST:
+                Contrast(ipl,radiusXY,radiusZ,thrMult,par1,par2,doIwhite);
+                break;
+
             case MEAN:
                 Mean(ipl,radiusXY,radiusZ,thrMult,par1,par2,doIwhite);
+                break;
+
+            case MEDIAN:
+                Median(ipl,radiusXY,radiusZ,thrMult,par1,par2,doIwhite);
                 break;
 
             case PHANSALKAR:
@@ -45,7 +55,7 @@ public class AutoLocalThreshold3D  implements PlugIn {
         }
     }
 
-    void Bernsen(ImagePlus ipl, int radiusXY,  int radiusZ, double thrMult, double par1, double par2, boolean doIwhite ) {
+    private void Bernsen(ImagePlus ipl, int radiusXY,  int radiusZ, double thrMult, double par1, double par2, boolean doIwhite ) {
         // Bernsen recommends WIN_SIZE = 31 and CONTRAST_THRESHOLD = 15.
         //  1) Bernsen J. (1986) "Dynamic Thresholding of Grey-Level Images"
         //    Proc. of the 8th Int. Conf. on Pattern Recognition, pp. 1251-1255
@@ -56,7 +66,7 @@ public class AutoLocalThreshold3D  implements PlugIn {
         // Ported to ImageJ plugin from E Celebi's fourier_0.8 routines
         // This version uses a circular local window, instead of a rectagular one
         ImagePlus maxIpl, minIpl;
-        int contrast_threshold=15;
+        int contrast_threshold = (int) Math.round(15*thrMult);
         int local_contrast;
         int mid_gray;
         byte object;
@@ -113,6 +123,56 @@ public class AutoLocalThreshold3D  implements PlugIn {
 
     }
 
+    private void Contrast(ImagePlus ipl, int radiusXY, int radiusZ, double thrMult,  double par1, double par2, boolean doIwhite) {
+        // G. Landini, 2013
+        // Based on a simple contrast toggle. This procedure does not have user-provided paramters other than the kernel radius
+        // Sets the pixel value to either white or black depending on whether its current value is closest to the local Max or Min respectively
+        // The procedure is similar to Toggle Contrast Enhancement (see Soille, Morphological Image Analysis (2004), p. 259
+
+        ImagePlus maxIpl, minIpl;
+        byte object;
+        byte backg;
+
+
+        if (doIwhite){
+            object =  (byte) 0xff;
+            backg =   (byte) 0;
+        }
+        else {
+            object =  (byte) 0;
+            backg =  (byte) 0xff;
+        }
+
+        IntensityMinMax.run(ipl,true);
+        IJ.run(ipl,"8-bit",null);
+
+        // Creating maximum image
+        maxIpl = new Duplicator().run(ipl);
+        maxIpl.setStack(Filters3D.filter(maxIpl.getImageStack(),Filters3D.MAX,radiusXY,radiusXY,radiusZ));
+
+        // Creating minimum image
+        minIpl = new Duplicator().run(ipl);
+        minIpl.setStack(Filters3D.filter(minIpl.getImageStack(),Filters3D.MIN,radiusXY,radiusXY,radiusZ));
+
+        for (int z = 1; z <= ipl.getNSlices(); z++) {
+            for (int c = 1; c <= ipl.getNChannels(); c++) {
+                for (int t = 1; t <= ipl.getNFrames(); t++) {
+                    ipl.setPosition(c, z, t);
+                    maxIpl.setPosition(c, z, t);
+                    minIpl.setPosition(c, z, t);
+
+                    byte[] pixels = (byte[]) ipl.getProcessor().getPixels();
+                    byte[] max = (byte[]) maxIpl.getProcessor().getPixels();
+                    byte[] min = (byte[]) minIpl.getProcessor().getPixels();
+
+                    for (int i = 0; i < pixels.length; i++) {
+                        pixels[i] = ((Math.abs((max[i] & 0xff - pixels[i] & 0xff)) <= thrMult*Math.abs((pixels[i] & 0xff - min[i] & 0xff)))) ? object : backg;
+                    }
+                }
+            }
+        }
+    }
+
     private void Mean(ImagePlus ipl, int radiusXY, int radiusZ,  double thrMult, double par1, double par2, boolean doIwhite ) {
         // See: Image Processing Learning Resourches HIPR2
         // http://homepages.inf.ed.ac.uk/rbf/HIPR2/adpthrsh.htm
@@ -154,13 +214,60 @@ public class AutoLocalThreshold3D  implements PlugIn {
                     float[] mean = (float[]) meanIpl.getProcessor().getPixels();
 
                     for (int i = 0; i < pixels.length; i++)
-                        pixels[i] = ((pixels[i] & 0xff) > (int) (mean[i] - c_value)) ? object : backg;
+                        pixels[i] = ((pixels[i] & 0xff) > (int) thrMult*(mean[i] - c_value)) ? object : backg;
                 }
             }
         }
 
         ipl.setPosition(1,1,1);
 
+    }
+
+    private void Median(ImagePlus ipl, int radiusXY, int radiusZ, double thrMult, double par1, double par2, boolean doIwhite ) {
+        // See: Image Processing Learning Resourches HIPR2
+        // http://homepages.inf.ed.ac.uk/rbf/HIPR2/adpthrsh.htm
+        ImagePlus medIpl;
+        int c_value = 0;
+        byte object;
+        byte backg;
+
+        if (par1!=0) c_value= (int) par1;
+
+        if (doIwhite){
+            object =  (byte) 0xff;
+            backg =   (byte) 0;
+        }
+        else {
+            object =  (byte) 0;
+            backg =  (byte) 0xff;
+        }
+
+        IntensityMinMax.run(ipl,true);
+        IJ.run(ipl,"8-bit",null);
+
+        // Normalising the image
+        medIpl = new Duplicator().run(ipl);
+
+        // Converting to 32-bit and normalising
+        IJ.run(medIpl,"32-bit",null);
+
+        // Applying 3D mean filter to meanIpl
+        medIpl.setStack(Filters3D.filter(medIpl.getImageStack(),Filters3D.MEDIAN,radiusXY,radiusXY,radiusZ));
+
+        for (int z = 1; z <= ipl.getNSlices(); z++) {
+            for (int c = 1; c <= ipl.getNChannels(); c++) {
+                for (int t = 1; t <= ipl.getNFrames(); t++) {
+                    ipl.setPosition(c, z, t);
+                    medIpl.setPosition(c, z, t);
+
+                    byte[] pixels = (byte[]) ipl.getProcessor().getPixels();
+                    byte[] median = (byte[]) medIpl.getProcessor().getPixels();
+
+                    for (int i = 0; i < pixels.length; i++)
+                        pixels[i] = ((pixels[i] & 0xff) > ((median[i] & 0xff) - c_value)) ? object : backg;
+                }
+            }
+        }
     }
 
     private void Phansalkar(ImagePlus ipl, int radiusXY, int radiusZ, double thrMult, double par1, double par2, boolean doIwhite) {
