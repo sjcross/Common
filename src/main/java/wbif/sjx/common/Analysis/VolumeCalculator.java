4ecf603829d6edc5966cbdba122cf09fb11b035f
Stephen Cross <stephencross86@gmail.com>
2018-06-18 16:54:54 +0100

    Started adding methods to get points contained by convex hull

--- src/main/java/wbif/sjx/common/Analysis/VolumeCalculator.java
+++ src/main/java/wbif/sjx/common/Analysis/VolumeCalculator.java
@@ -2,6 +2,13 @@ package wbif.sjx.common.Analysis;
 
 import com.github.quickhull3d.Point3d;
 import com.github.quickhull3d.QuickHull3D;
+import org.apache.commons.math3.geometry.euclidean.threed.Euclidean3D;
+import org.apache.commons.math3.geometry.euclidean.threed.Plane;
+import org.apache.commons.math3.geometry.euclidean.threed.PolyhedronsSet;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.geometry.partitioning.BSPTree;
+import org.apache.commons.math3.geometry.partitioning.Region;
+import org.apache.commons.math3.geometry.partitioning.RegionFactory;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -10,6 +17,10 @@ import org.slf4j.LoggerFactory;
 import wbif.sjx.common.MathFunc.GeneralOps;
 import wbif.sjx.common.Object.Volume;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 
 public class VolumeCalculator {
@@ -171,6 +182,61 @@ public class VolumeCalculator {
         if (Double.isNaN(hullVolume)) getHullVolume(true);
 
         return volume.getContainedVolume(true)/getHullVolume(true);
+
+    }
+
+    public Volume getContainedPoints() {
+        if (!canFitHull()) return null;
+
+        // Getting a list of vertices
+        Point3d[] vertices = hull.getVertices();
+        LinkedList<Vector3D> vectorList = new LinkedList<>();
+        for (Point3d point3d:vertices) {
+            vectorList.add(new Vector3D(point3d.x,point3d.y,(point3d.z/5)));
+            System.out.println("    point: "+point3d.x+"_"+point3d.y+"_"+(point3d.z/5));
+        }
+
+        // Getting a list of facets
+        int[][] facets = hull.getFaces(QuickHull3D.CLOCKWISE);
+        LinkedList<int[]> facetsList = new LinkedList<>();
+        for (int[] facet:facets) {
+            facetsList.add(facet);
+            System.out.println("    facet: "+facet[0]+"_"+facet[1]+"_"+facet[2]);
+        }
+//        facetsList.addAll(Arrays.asList(facets));
+
+        // Creating the PolyhedronsSet
+        PolyhedronsSet polyhedronsSet = new PolyhedronsSet(vectorList,facetsList,1E-2);
+
+        // Testing which points are within the convex hull
+        double[] extents = volume.getExtents(true,false);
+        Volume insideHull = new Volume(volume.getDistPerPxXY(),volume.getDistPerPxZ(),volume.getCalibratedUnits(),volume.is2D());
+        for (int x=(int) extents[0];x<=extents[1]+1;x++) {
+            for (int y=(int) extents[2];y<=extents[3];y++) {
+                for (int z=(int) extents[4];z<=extents[5];z++) {
+                    Region.Location location = polyhedronsSet.checkPoint(new Vector3D(1,1,1));
+                    if (location.equals(Region.Location.INSIDE)) {
+                        insideHull.addCoord(x,y,z);
+                        System.out.println("    Inside "+x+"_"+y+"_"+z);
+                    } else {
+                        System.out.println("    Outside "+x+"_"+y+"_"+z);
+                    }
+                }
+            }
+        }
+
+        RegionFactory<Euclidean3D> regionFactory = new RegionFactory<>();
+        ArrayList<Plane> planes = new ArrayList<>();
+        
+
+        PolyhedronsSet tree =
+                (PolyhedronsSet) new RegionFactory<Euclidean3D>().buildConvex(
+                        new Plane(vertex3, vertex2, vertex1, 1.0e-10),
+                        new Plane(vertex2, vertex3, vertex4, 1.0e-10),
+                        new Plane(vertex4, vertex3, vertex1, 1.0e-10),
+                        new Plane(vertex1, vertex2, vertex4, 1.0e-10));
+
+        return insideHull;
 
     }
 }
